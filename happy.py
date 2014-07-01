1e921ea725c5834e6dd5a35e7fa94d3f77e26c65
luisgustavoneves <luisgustavoneves@gmail.com>
2014-07-01 18:35:42 -0300

    Update happy.py
    Bug fixes and performance improve for hgraph.

--- happy.py
+++ happy.py
@@ -4,28 +4,24 @@ LANG = 'english'
 wordlist = open('labMTwords-%s.csv' % LANG,'r').read().split('\n')
 scores = open('labMTscores-%s.csv' % LANG,'r').read().split('\n')
 
-import nltk
-from nltk.tokenize import RegexpTokenizer
-TOKENIZER = RegexpTokenizer('(?u)\W+|\$[\d\.]+|\S+')
-SPECIAL_CHARS = ['.', ',', '!', '?']
+import re
+
+SPECIAL_CHARS = ['.', ',', '!', '?', '\n']
 
 def get_words(text=''):
-    words = []
+    new_text = text
     pos = []
     lastpos = 0
-    words = TOKENIZER.tokenize(text)
-    filtered_words = []
+    for char in SPECIAL_CHARS:
+        new_text  = new_text.replace(char, ' ')
+    new_text = re.sub(' +', ' ', new_text)
+    words = new_text.split(' ')
+
     for word in words:
-        if word in SPECIAL_CHARS or word == " ":
-            pass
-        else:
-            new_word = word.replace(",","").replace(".","")
-            new_word = new_word.replace("!","").replace("?","")
-            lastpos =  text[lastpos:].find(new_word) + lastpos
-            filtered_words.append(new_word)
-            pos.append(lastpos)
-            lastpos = lastpos + len(new_word)
-    return filtered_words, pos
+        lastpos =  text.find(word, lastpos)
+        pos.append(lastpos)
+        lastpos = lastpos + len(word) -1
+    return words, pos
 
 def hi(s, ws=None):
 
@@ -59,7 +55,7 @@ def hgraph(s, window):
     ws, pos = get_words(text)
 
     i = 0
-    h = []
+    hs = []
 
     maxhi = 0
     minhi = 9999999999999999
@@ -68,7 +64,33 @@ def hgraph(s, window):
 
     while(i+window<=len(ws)):
 
-        whi = hi('',ws[i:i+window])
+        #whi = hi('',ws[i:i+window])
+
+        if i>0:
+            if ws[i-1] in wordlist and (float(scores[wordlist.index(ws[i-1])].strip())<=3 or float(scores[wordlist.index(ws[i-1])].strip())>=7):
+                df[ws[i-1]] = df[ws[i-1]] -1
+                if df[ws[i-1]] == 0:
+                    df.pop(ws[i-1])
+            if ws[i+window-1] in wordlist and (float(scores[wordlist.index(ws[i+window-1])].strip())<=3 or float(scores[wordlist.index(ws[i+window-1])].strip())>=7):
+                df[ws[i+window-1]] = df.get(ws[i+window-1],0)+1
+
+        else:
+
+            df = {}
+            for w in ws[i:i+window]:
+                 if w in wordlist and (float(scores[wordlist.index(w)].strip())<=3 or float(scores[wordlist.index(w)].strip())>=7):
+                    df[w] = df.get(w,0)+1
+
+        h = 0.
+        f = 0
+        for k in df.keys():
+            h = h + df[k]*float(scores[wordlist.index(k)].strip())
+            f = f + df[k]
+
+        if f>0:
+            whi= h/f
+        else:
+            whi= None
 
         if whi:
             if whi > maxhi:
@@ -77,11 +99,11 @@ def hgraph(s, window):
             if whi < minhi:
                 minhi= whi
                 mini= i
-            h.append(whi)
+            hs.append(whi)
 
         i = i + 1
 
-    h.append(hi('',ws[i:]))
+    hs.append(hi('',ws[i:]))
 
     # this helps figuring out if a small window is meaningful
     print "Max score sequence:", maxhi
@@ -90,8 +112,9 @@ def hgraph(s, window):
     print "Min score sequence:", minhi
     print s[pos[mini]:pos[mini+window]+len(ws[mini+window])]
 
-    plt.plot(h)
-    plt.show()
+    plt.plot(hs)
+    plt.savefig('hgraph.png')
+
 
 if __name__ == "__main__":
 
@@ -104,7 +127,10 @@ if __name__ == "__main__":
     s3 = '''Stop Crying Your Heart Out (...)
 '''
 
+    s4 = open('prideandprejudice.txt','r').read()
+
+
     print hi(s1)
     print hi(s2)
     print hi(s3)
-    hgraph(s3,20)
+    hgraph(s4,10000) 
